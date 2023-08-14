b54fb3dac8832817a74f46a3f86e92439155d94c
sergiogenes <genessergio@gmail.com>
2023-08-14 19:08:15 -0300

    Se está modificando el componente Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,46 @@
+import { useRef, useState } from 'react'
 import logo from '../../assets/logo.svg'
-import PassengerSelect from '../PasangerSelect/PasangerSelect'
-import { SearchBar } from '../SearchBar'
 import './index.css'
 
 export const Navbar = () => {
+  const [wide, setWide] = useState(false)
+  const navBarRef = useRef<HTMLDivElement>(null)
+
+  const handleWide = () => {
+    if (!wide) {
+      navBarRef.current?.classList.add('wide')
+      setWide(true)
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    navBarRef.current?.classList.remove('wide')
+    setWide(false)
+  }
+
   return (
-    <nav>
+    <nav ref={navBarRef}>
       <img src={logo} alt='image of the logo' />
-      <SearchBar />
+      <div className='search-bar-container' onClick={handleWide}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='location'>
+            <p>Location</p>
+            <input
+              type='text'
+              id='location'
+              defaultValue={'Helsinki, Finland'}
+            />
+          </label>
+          <label htmlFor='guests'>
+            <p>Guests</p>
+            <input type='text' id='guests' placeholder='Add guests' />
+          </label>
+          <button type='submit'>
+            <span className='material-icons'>search</span>
+          </button>
+        </form>
+      </div>
     </nav>
   )
 }
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,9 +1,26 @@
+import React, { useRef, useState } from 'react'
 import './index.css'
 
 export const SearchBar = () => {
+  const [wide, setWide] = useState(false)
+  const searchBar = useRef<HTMLDivElement>(null)
+
+  const handleWide = (event: React.MouseEvent) => {
+    if (!wide) {
+      searchBar.current?.classList.add('wide')
+      setWide(true)
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    searchBar.current?.classList.remove('wide')
+    setWide(false)
+  }
+
   return (
-    <div className='search-bar-container'>
-      <form>
+    <div className='search-bar-container' ref={searchBar} onClick={handleWide}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='location'>
           <p>Location</p>
           <input type='text' id='location' defaultValue={'Helsinki, Finland'} />
